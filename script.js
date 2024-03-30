0df0d33420ddc31033138a69a4bd23cb03df8d2e
Tolu A <mrtalder@gmail.com>
2024-03-30 11:12:47 +0000

    Remove the function that plays 5 rounds

--- script.js
+++ script.js
@@ -45,34 +45,4 @@ function singleRound(playerSelection, computerSelection){
     }
 }
 
-/// A function that plays 5 rounds of rock paper scissors
-function playGame() {
-    let computerScore = 0;
-    let playerScore = 0;
-
-    for (i = 0; i < 5; i++){
-        computerSelection = getComputerChoice();
-        playerSelection = getPlayerChoice();
-        
-        if (playerSelection === null) {
-            return "Game cancelled by the player.";
-        }
-        
-        let result = singleRound(playerSelection, computerSelection)
-        if (result === "You win"){
-            playerScore += 1;
-        }
-        else if (result === "Computer wins"){
-            computerScore += 1;
-        }
-        console.log(result);
-    }
-    if(playerScore > computerScore){
-        return `You win ${playerScore} to ${computerScore}`;
-    }
-    else if (computerScore > playerScore){
-        return `Computer wins ${computerScore} to ${playerScore}`;;
-    }
-}
-
 console.log(playGame());
